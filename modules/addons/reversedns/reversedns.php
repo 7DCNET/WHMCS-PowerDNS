93d93b0311990705589cb3c6df5a4b7665c048c6
7DC.NET <ozkan@7dc.net>
2024-09-07 16:36:55 +0300

    Update reversedns.php

--- modules/addons/reversedns/reversedns.php
+++ modules/addons/reversedns/reversedns.php
@@ -1,27 +1,27 @@
 <?php
 if (!defined("WHMCS")) {
-    die("Bu dosya doğrudan erişilemez");
+    die("Bu dosyaya doğrudan erişim sağlanamaz.");
 }
 
 function reversedns_config() {
     $configarray = array(
         "name" => "Reverse DNS Yönetimi",
         "description" => "Müşterilerin atanmış IP adresleri için reverse DNS kayıtlarını güncelleyebilmelerini sağlar.",
-        "version" => "1.0",
+        "version" => "1.1",
         "author" => "Sizin İsminiz",
         "fields" => array(
             "api_url" => array(
                 "FriendlyName" => "PowerDNS API URL",
                 "Type" => "text",
                 "Size" => "50",
-                "Default" => "http://46.253.7.3:8082/api/v1",
+                "Default" => "http://46.253.7.3:8081/api/v1",
                 "Description" => "PowerDNS API'nin temel URL'si",
             ),
             "api_key" => array(
                 "FriendlyName" => "PowerDNS API Anahtarı",
                 "Type" => "password",
                 "Size" => "50",
-                "Default" => "LcCSBg6mF9rBSLHf",
+                "Default" => "changeme",
                 "Description" => "PowerDNS API anahtarınızı girin",
             ),
         ),
@@ -38,26 +38,32 @@ function reversedns_deactivate() {
 }
 
 function reversedns_clientarea($vars) {
-    $userid = $_SESSION['uid'];
+    $userid = $_SESSION['uid']; // Oturum açmış kullanıcının ID'sini al
     $apiUrl = $vars['api_url'];
     $apiKey = $vars['api_key'];
 
-    // Müşterinin atanmış IP adreslerini al
+    // Müşterinin aktif ürünlerini al
     $result = localAPI('GetClientsProducts', array('clientid' => $userid));
-    $ipAddresses = array();
-
-    foreach ($result['products']['product'] as $product) {
-        $dedicatedip = $product['dedicatedip'];
-        if (!empty($dedicatedip)) {
-            $ips = explode(',', $dedicatedip);
-            foreach ($ips as $ip) {
-                $ipAddresses[] = trim($ip);
+    if (isset($result['products']['product'])) {
+        $ipAddresses = array();
+        foreach ($result['products']['product'] as $product) {
+            if ($product['status'] === 'Active') {
+                $dedicatedip = $product['dedicatedip'];
+                if (!empty($dedicatedip)) {
+                    $ips = explode(',', $dedicatedip);
+                    foreach ($ips as $ip) {
+                        $ipAddresses[] = trim($ip);
+                    }
+                }
             }
         }
+    } else {
+        $error = 'IP adresleri alınamadı.';
+        $ipAddresses = array();
     }
 
     // Form gönderildi mi?
-    if ($_POST['update_rdns']) {
+    if (isset($_POST['update_rdns'])) {
         $ipToUpdate = $_POST['update_rdns'];
         $newRDNS = $_POST['new_rdns'][$ipToUpdate];
 
@@ -70,6 +76,7 @@ function reversedns_clientarea($vars) {
                         'name' => getPTRDomain($ipToUpdate),
                         'type' => 'PTR',
                         'changetype' => 'REPLACE',
+                        'ttl' => 3600, // TTL değerini ekleyin
                         'records' => array(
                             array(
                                 'content' => $newRDNS . '.',
@@ -85,19 +92,22 @@ function reversedns_clientarea($vars) {
                 'X-API-Key: ' . $apiKey,
                 'Content-Type: application/json',
             ));
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($ptrRecord));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($ptrRecord));
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
 
             $response = curl_exec($ch);
             $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-
-            if ($httpCode == 200) {
+            if (curl_errno($ch)) {
+                $error = 'CURL Error: ' . curl_error($ch);
+            } else if ($httpCode == 204) {
                 $success = 'Reverse DNS kaydı başarıyla güncellendi.';
+                // Başarı mesajını gösterip yönlendirmeyi JavaScript ile yapacağız
+                $redirect = true;
             } else {
-                $error = 'Güncelleme sırasında bir hata oluştu.';
+                $error = 'Güncelleme sırasında bir hata oluştu. HTTP Kodu: ' . $httpCode . ' Yanıt: ' . $response;
             }
+            curl_close($ch);
         } else {
             $error = 'Geçersiz IP adresi veya hostname.';
         }
@@ -112,11 +122,19 @@ function reversedns_clientarea($vars) {
             'X-API-Key: ' . $apiKey,
         ));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
         $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if (curl_errno($ch)) {
+            $error = 'CURL Error: ' . curl_error($ch);
+        } else if ($httpCode != 200) {
+            $error = 'Zone bilgisi alınamadı. HTTP Kodu: ' . $httpCode . ' Yanıt: ' . $response;
+            $zoneData = array(); // Zone bilgisi alınamadıysa boş dizi atama
+        } else {
+            $zoneData = json_decode($response, true);
+        }
         curl_close($ch);
-
-        $zoneData = json_decode($response, true);
 
         $currentRDNS = '';
         if (isset($zoneData['rrsets'])) {
@@ -134,16 +152,19 @@ function reversedns_clientarea($vars) {
     }
 
     // Şablona verileri gönder
+    $vars = array(
+        'ipaddresses' => $ipData,
+        'error' => isset($error) ? $error : '',
+        'success' => isset($success) ? $success : '',
+        'redirect' => isset($redirect) ? $redirect : false,
+    );
+
     return array(
         'pagetitle' => 'Reverse DNS Yönetimi',
         'breadcrumb' => array('index.php?m=reversedns' => 'Reverse DNS Yönetimi'),
         'templatefile' => 'clientarea',
         'requirelogin' => true,
-        'vars' => array(
-            'ipaddresses' => $ipData,
-            'error' => isset($error) ? $error : '',
-            'success' => isset($success) ? $success : '',
-        ),
+        'vars' => $vars,
     );
 }
 
@@ -154,6 +175,12 @@ function getPTRDomain($ip) {
 
 function getZoneName($ip) {
     $octets = explode('.', $ip);
-    return $octets[0] . '.' . $octets[1] . '.' . $octets[2] . '.in-addr.arpa.';
+    if (count($octets) == 4) {
+        // Bu örnek, sadece /24 subnet maskesi için doğru çalışır.
+        return $octets[2] . '.' . $octets[1] . '.' . $octets[0] . '.in-addr.arpa.';
+    }
+    return ''; // Geçersiz IP formatı
 }
+
+
 ?>
